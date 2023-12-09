08d86825b036c9921f6f926092dea77dc89ada81
EduardoMaltauro <maltauroeduardo717@gmail.com>
2023-12-09 12:01:01 -0300

    Electron

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow, ipcMain } = require("electron")
+const { join } = require("path")
+
+app.whenReady()
+  .then(function() {
+    const janela = new BrowserWindow({
+        autoHideMenuBar: true,
+        frame: false,
+        icon: join(__dirname, "/public/icon.png"),
+        minHeight: 600,
+        minWidth: 512,
+        resizable: false,
+        title: "Station",
+        webPreferences: {
+          preload: join(__dirname, "preload.js")
+        }
+      })
+
+      janela.loadFile( join(__dirname, "public/PaginaInicio.html") )
+  })
